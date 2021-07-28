83390a3cd55257e516675c5efc54dcce6029304f
Leonieke <54051901+datwatikmaak@users.noreply.github.com>
2021-07-28 04:47:03 +0200

    Added a test file that works with fixtures

---
+++ test_div_by_3_6.py
@@ -0,0 +1,14 @@
+import pytest
+
+
+@pytest.fixture
+def input_value():
+    return 39
+
+
+def test_divisible_by_3(input_value):
+    assert input_value % 3 == 0
+
+
+def test_divisible_by_6(input_value):
+    assert input_value % 6 == 0
